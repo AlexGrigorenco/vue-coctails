7592945f549aba482e1327e61da28a0a6ed591f1
AlexGrigorenco <alpsnhandy@gmail.com>
2023-12-18 01:29:01 +0200

    wrote func to get ingredients and seved in pinia

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,17 +1,35 @@
 <script setup>
     import AppLayout from "@/components/AppLayout.vue";
+    import {useRootStore} from '@/stores/root';
+    import { storeToRefs } from "pinia";
+
+    const rootStore = useRootStore();
+    rootStore.getIngredients()
+
+    const {ingredients} = storeToRefs(rootStore)
 </script>
 
 
 <template>
-    <AppLayout imgName="home-bg.jpg"  />
+    <AppLayout imgName="home-bg.jpg" >
+        <div class="wrapper container">
+            <div class="title">
+                Choose your drink
+            </div>
+            <div class="line"></div>
+        </div>
+    </AppLayout>
 </template>
 
 
 <style lang="scss" scoped>
 @import '../assets/styles/main.scss';
 
-.ral {
-    font-family: 'Raleway', 'Arial', sans-serif;
+.wrapper {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    min-height: 100%;
 }
 </style>
