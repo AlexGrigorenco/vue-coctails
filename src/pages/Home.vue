8aaf96743e0aac2569e37758d35d9a14cc7f0477
AlexGrigorenco <alpsnhandy@gmail.com>
2023-12-18 02:45:52 +0200

    added select component

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,35 +1,53 @@
 <script setup>
-    import AppLayout from "@/components/AppLayout.vue";
-    import {useRootStore} from '@/stores/root';
-    import { storeToRefs } from "pinia";
+import { ref } from "vue";
+import AppLayout from "@/components/AppLayout.vue";
+import { useRootStore } from "@/stores/root";
+import { storeToRefs } from "pinia";
 
-    const rootStore = useRootStore();
-    rootStore.getIngredients()
+const rootStore = useRootStore();
+rootStore.getIngredients();
 
-    const {ingredients} = storeToRefs(rootStore)
+const { ingredients } = storeToRefs(rootStore);
+const ingredient = ref(null);
 </script>
 
 
 <template>
-    <AppLayout imgName="home-bg.jpg" >
-        <div class="wrapper container">
-            <div class="title">
-                Choose your drink
-            </div>
-            <div class="line"></div>
-        </div>
-    </AppLayout>
+  <AppLayout imgName="home-bg.jpg">
+    <div class="wrapper container">
+      <div class="title">Choose your drink</div>
+      <div class="line"></div>
+      <div class="select-wrapper">
+        <el-select
+          v-model="ingredient"
+          class="select"
+          placeholder="Choose main ingredient"
+          size="large"
+        >
+          <el-option
+            v-for="item in ingredients"
+            :key="item.strIngredient1"
+            :label="item.strIngredient1"
+            :value="item.strIngredient1"
+          />
+        </el-select>
+      </div>
+    </div>
+  </AppLayout>
 </template>
 
 
 <style lang="scss" scoped>
-@import '../assets/styles/main.scss';
+@import "../assets/styles/main.scss";
 
 .wrapper {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    flex-direction: column;
-    min-height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  min-height: 100%;
+}
+.select-wrapper {
+    padding: 60px 0;
 }
 </style>
